c6033ebb5a6769eee83025ea9344da5ab3b14fc9
muratzenkin <muratzenkin94@gmail.com>
2025-03-24 14:15:19 +0100

    update

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,29 +1,17 @@
-// import { useState } from "react";
-// import MuratPortfolio from "./components/MyPortfolio";
-// import IntroModal from "./components/IntroModal";
-
-// function App() {
-//   const [showIntro, setShowIntro] = useState(true);
-
-//   return (
-//     <div className="min-h-screen bg-gray-200 flex justify-center items-center p-8">
-//       {/* Açılış Animasyonu */}
-//       {showIntro && <IntroModal onClose={() => setShowIntro(false)} />}
-
-//       {/* Ana İçerik */}
-//       {!showIntro && <MuratPortfolio />}
-//     </div>
-//   );
-// }
-
-// export default App;
-
+import { useState } from "react";
 import MuratPortfolio from "./components/MyPortfolio";
+import IntroModal from "./components/IntroModal";
 
 function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
   return (
     <div className="min-h-screen bg-gray-200 flex justify-center items-center p-8">
-      <MuratPortfolio />
+      {/* Açılış Animasyonu */}
+      {showIntro && <IntroModal onClose={() => setShowIntro(false)} />}
+
+      {/* Ana İçerik */}
+      {!showIntro && <MuratPortfolio />}
     </div>
   );
 }
